615841dadcddc22d7799b6a62f3f83ca0840a9c6
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-06-28 11:29:47 +0200

    Add css language server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,34 +1,7 @@
-local cmp = require('cmp')
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn['vsnip#anonymous'](args.body)
-		end,
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' },
-		{ name = 'buffer' },
-	},
-})
-
-cmp.setup.cmdline('/', {
-	sources = {
-		{ name = 'buffer' },
-	},
-})
-
-cmp.setup.cmdline(':', {
-	sources = {
-		{ name = 'path' },
-		{ name = 'cmdline' },
-	},
-})
-
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lspconfig = require('lspconfig')
+lspconfig.cssls.setup({ capabilities = capabilities, cmd = { 'vscode-css-languageserver', '--stdio' } })
+lspconfig.eslint.setup({ capabilities = capabilities })
 lspconfig.pyright.setup({ capabilities = capabilities })
 lspconfig.tsserver.setup({ capabilities = capabilities })
-lspconfig.eslint.setup({})
--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,8 +1,32 @@
 local cmp = require('cmp')
+
 cmp.setup({
 	mapping = {
 		['<c-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
 		['<c-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
 		['<c-l>'] = cmp.mapping(cmp.mapping.confirm(), { 'i', 'c' }),
+	},
+	snippet = {
+		expand = function(args)
+			vim.fn['vsnip#anonymous'](args.body)
+		end,
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' },
+		{ name = 'buffer' },
+	},
+})
+
+cmp.setup.cmdline('/', {
+	sources = {
+		{ name = 'buffer' },
+	},
+})
+
+cmp.setup.cmdline(':', {
+	sources = {
+		{ name = 'path' },
+		{ name = 'cmdline' },
 	},
 })
