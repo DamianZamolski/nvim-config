0484fb2f33f1668c0a89a2e41c2b2403b00237d9
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-09-23 10:03:01 +0200

    Add treesitter-context

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,7 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lspconfig = require('lspconfig')
+lspconfig.bashls.setup({ capabilities = capabilities })
 lspconfig.cssls.setup({ capabilities = capabilities, cmd = { 'vscode-css-languageserver', '--stdio' } })
 lspconfig.eslint.setup({ capabilities = capabilities })
 
